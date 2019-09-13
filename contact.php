124fb150d9ac8c57aca56e19b5adba1bf02ebe3b
unknown <matthias.cartel@gmail.com>
2019-09-13 17:34:09 +0200

    Mail Sender

---
+++ contact.php
@@ -0,0 +1,26 @@
+<?php
+if(isset($_POST['submit']))
+{
+$name = $_POST['name'];
+$visitoremail = $_POST['email'];
+$message = $_POST['message'];
+
+
+$email_from = 'matthias.cartel@laposte.net';
+
+$email_subject = "New mail";
+
+$email_body = "User Name: $name. \n". 
+                "User Email: $visitoremail.\n". 
+                    "User message: $message.\n";
+
+$to = "matthias.cartel@gmail.com";
+
+$headers = "From: $email_from \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: index.html?mailSend");
+
+}
+?>
